83d8920746a003bb03a19e879f1dd90764eb9040
Zuraiz Ahmed Shehzad <02-132172-048@student.bahria.edu.pk>
2025-03-22 03:29:21 +0500

    40. Implementing Get All Products Handler

---
+++ Services/Catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,88 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository, IBrandRepository, ITypesRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context;
+        }
+        async Task<Product> IProductRepository.CreateProduct(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+            return product;
+        }
+
+        async Task<bool> IProductRepository.DeleteProduct(string id)
+        {
+            var deletedProduct = await _context
+                .Products
+                .DeleteOneAsync(p => p.Id == id);
+
+            return deletedProduct.IsAcknowledged && deletedProduct.DeletedCount > 0;
+        }
+
+        async Task<IEnumerable<ProductBrand>> IBrandRepository.GetAllBrands()
+        {
+            return await _context
+                .Brands
+                .Find(brand => true)
+                .ToListAsync();
+        }
+
+        async Task<IEnumerable<ProductType>> ITypesRepository.GetAllTypes()
+        {
+            return await _context
+                .Types
+                .Find(type => true)
+                .ToListAsync();
+        }
+
+        async Task<Product> IProductRepository.GetProduct(string id)
+        {
+            return await _context
+                .Products
+                .Find(p=>p.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        async Task<IEnumerable<Product>> IProductRepository.GetProductByBrand(string brand)
+        {
+            return await _context
+                .Products
+                .Find(p=>p.Brands.Name.ToLower() == brand.ToLower())
+                .ToListAsync();
+        }
+
+        async Task<IEnumerable<Product>> IProductRepository.GetProductByName(string name)
+        {
+            return await _context
+                .Products
+                .Find(p => p.Name.ToLower() == name.ToLower())
+                .ToListAsync();
+        }
+
+        async Task<IEnumerable<Product>> IProductRepository.GetProducts()
+        {
+            return await _context
+                .Products
+                .Find(p=>true)
+                .ToListAsync();
+        }
+
+        async Task<bool> IProductRepository.UpdateProduct(Product product)
+        {
+            var updatedProduct = await _context
+                .Products
+                .ReplaceOneAsync(p => p.Id == product.Id, product);
+
+            return updatedProduct.IsAcknowledged && updatedProduct.ModifiedCount > 0;
+        }
+    }
+}
